a4c056660447cc4794aad8941291cac8fe41172c
Virumandi <144411002+Virumandi@users.noreply.github.com>
2023-09-22 13:59:24 +0530

    Update bank account.py

--- bank account.py
+++ bank account.py
@@ -1,4 +1,4 @@
-#1-Write a function called linear_search_product that takes the list of products and a target product name as input. The function should perform a linear search to find the target product in the list and return a list of indices of all occurrences of the product if found, or an empty list if the product is not found.
+#1Implement a class called BankAccount that represents a bank account. The class should have private attributes for account number, account holder name, and account balance. Include methods to deposit money, withdraw money, and display the account balance. Ensure that the account balance cannot be accessed directly from outside the class. Write a program to create an instance of the BankAccount class and test the deposit and withdrawal functionality.
 class BankAccount:
     def __init__(self, account_number, account_holder_name, initial_balance):
         self._account_number = account_number
